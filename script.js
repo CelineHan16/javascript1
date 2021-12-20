c5a642a3aefad3be561aa8ef3f92d5d67557c264
syrgo <syrga.v30@gmail.com>
2021-12-20 15:59:43 +0300

    created new file js

---
+++ script.js
@@ -0,0 +1,73 @@
+// alert("hello world")
+
+// console.log("Hello s")
+
+// declaration
+// var firstName;
+// var lastName;
+// var age;
+// var school;
+// var address;
+ 
+
+// initialize
+var firstName = "Syrga";
+var lastName = "Asanbekova";
+var age = 17;
+var school = "#11";
+var grade = "11v";
+var address = "Abdraimova 9";
+var birthDate = "04.16.2004";
+var favoriteColor = "purple";
+var status = "student";
+var hobby = "English";
+var mother = "Nurgul";
+var father = "Almanbet";
+var brothers = "Askar and Kulchoro";
+var sisters = "Sabina and Nazik";
+var besties = "Nazik, Taazim and Jenn";
+var favoriteMusic = "pop, k-pop";
+var languages = "kyrgyz, russian and english";
+var country = "Kyrgyzstan";
+var number = "+996999665712";
+var instagram = "stka_.25";
+var specialty = "IT, Junior front-end developper";
+
+
+// assignment
+// firstname = "syrga";
+
+// example 1 math
+var potatoes = 5, tomatoes = 3;
+var vegetables = potatoes + tomatoes;
+var result = vegetables / 2;
+// console.log(result);
+
+// example 2 math
+result = (2 + 1) / 3 - 6 * 2; //-11
+result = 5 - 1 - 10 * 3; //-36 
+result = 19 * (2 / 5) + 64;
+// console.log(result);
+
+// example 3 math
+var a = 4;
+var b = 6;
+var squareArea = a * a;
+// console.log(squareArea);
+var rectangleArea = a * b;
+// console.log(rectangleArea);
+var a = 4;
+var b = 9;
+var c = 6**2;
+var x = 8;
+var equal = c + a - (b * x);
+console.log(equal);
+
+let eight =  Math.sqrt(4);
+
+
+// example 4 math
+var PI = 3.14;
+var r = 5;
+var circleArea = r * r * PI;
+alert(eight);
