8be372f03d9fc9834297e1b972f1008796115118
syrgo <syrga.v30@gmail.com>
2021-12-22 16:06:44 +0300

    created new alerts

--- script.js
+++ script.js
@@ -61,7 +61,8 @@ var b = 9;
 var c = 6**2;
 var x = 8;
 var equal = c + a - (b * x);
-console.log(equal);
+// console.log(equal);
+
 
 let eight =  Math.sqrt(4);
 
@@ -70,4 +71,110 @@ let eight =  Math.sqrt(4);
 var PI = 3.14;
 var r = 5;
 var circleArea = r * r * PI;
-alert(eight);
+// alert(eight);
+
+var example1 = 10 === 10; // equals
+var example2 = 11 > 10; // more than
+var example3 = 10 < 11; // less than 
+var example4 = 10 >= 10; // more than or equal
+var example5 = 10 <= 10; // less than or equal
+var example6 = 11 !== 10; // not equal
+var example7 = "A" < "Z"; 
+var example8 = "A" === "A";
+var example9 = "AA" < "AB";
+
+// // numbers
+// // 1, 2, 3, 4, 5, 6, 10.1
+// var number1 = 1;
+// var number2 = 2.3;
+// var number4 = 5;
+
+// // string, text
+// var string1 = "Hi world";
+// var string2 = "A";
+// var string3 = "";
+// var string4 = "5";
+
+// // boolean
+// var boolean1 = true; //1
+// var boolean2 = false; // 0
+
+// var a = 15;
+// var b = 10;
+// var example10 = a < b;
+
+// var c = 10;
+// var d = 11;
+// if (c < d) { // TRUE
+//   // alert("C is less than D");
+//   console.log('Hello world');
+// } 
+
+// var e = 3.14;
+// if (e !== 3.14) {
+//   alert("Why don't you know number PI?"); // FALSE
+// }
+// else {
+//   alert("Good job!");
+// }
+
+// var age = 10;
+// if (age >= 18) {
+//   alert("You can vote!"); //TRUE
+// }
+// else {
+//   alert("You are too young to vote!"); // FALSE
+// }
+
+// var lightSwitch = 1;
+// if (lightSwitch === 1) {
+//   alert("Light is on"); // TRUE
+// }
+// else {
+//   alert("Light is off"); //FALSE
+// }
+
+// if (confirm("Do you have a car?")) {
+//   alert("Then this website is not for you!");
+// }
+// else {
+//   alert("Welcome fellow pedestrian!");
+// }
+
+// var username = prompt ("What is tour name?", "stranger");
+// alert("Welcome " + username);
+
+// var a = prompt("What is the width of rectangle?");
+// var b = prompt("What is the height of rectangle?");
+// alert(a + b);
+
+// if (confirm("Are you looking for a party?")) {
+//   alert("Come to hang out with us!");
+// }
+// else {
+//   alert("You are always welcome, if u have change of heart)")
+// }
+
+if (confirm("Are you army?")) {
+  if (confirm("Do you want to get a ticket to fanmeeting?")) {
+    if (confirm("Looking for a buddy to go with?")) {
+      alert("You are in right way!");
+    }
+    else {
+      if(confirm("Looking for a content with BTS?")) {
+        alert("Welcome to our purple island!");
+      }
+      else {
+        alert("What are you doing here?")
+      }
+    }
+  }
+}  
+else {
+  if (confirm("Then maybe stay?")) {
+    alert("Let's go to their concert!");
+  }
+  else {
+    alert("This site is not for you :(");
+  }
+}
